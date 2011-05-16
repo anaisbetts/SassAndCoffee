b2f801aebd20bffd6d3870cb4c30de9afe1a6d01
Paul Betts <paul@paulbetts.org>
2011-05-15 21:34:47 -0700

    Add Test project

---
+++ SassAndCoffee.Tests/CoffeeScriptCompilerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SassAndCoffee.Tests
+{
+    public class CoffeeScriptCompilerTest
+    {
+        [Fact]
+        public void CoffeeScriptSmokeTest()
+        {
+            var input = @"v = x*5 for x in [1...10]";
+            var fixture = new CoffeeScriptCompiler();
+
+            var result = fixture.Compile(input);
+            Assert.False(String.IsNullOrWhiteSpace(result));
+        }
+    }
+}
