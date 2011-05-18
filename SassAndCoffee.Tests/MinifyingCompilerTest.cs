261315b3a70bfe0104916f9729e405b7a7d70927
Paul Betts <paul@paulbetts.org>
2011-05-17 23:49:50 -0700

    Add UglifyJs minification compiler

---
+++ SassAndCoffee.Tests/MinifyingCompilerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SassAndCoffee.Tests
+{
+    public class MinifyingCompilerTest
+    {
+        [Fact]
+        public void UglifySmokeTest()
+        {
+            var fixture = new MinifyingCompiler();
+            var input = "var someLongVariableName = 4;";
+            string output = fixture.Compile(input);
+
+            Console.WriteLine("Input: '{0}', Output: '{1}'", input, output);
+            Assert.True(output.Length < input.Length);
+        }
+    }
+}
---
+++ SassAndCoffee/MinifyingCompiler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Jurassic;
+
+namespace SassAndCoffee
+{
+    public class MinifyingCompiler
+    {
+        ScriptEngine _engine;
+        ScriptSource _uglifyJsCode;
+
+        public string Compile(string javascriptCode)
+        {
+            if (_engine == null) {
+                _engine = initializeMinificationEngine();
+            }
+
+            return _engine.CallGlobalFunction<string>("squeeze_it", javascriptCode);
+        }
+
+        ScriptEngine initializeMinificationEngine()
+        {
+            if (this._uglifyJsCode == null) {
+                this._uglifyJsCode = new StringScriptSource(Utility.ResourceAsString("SassAndCoffee.lib.uglify.js"));
+            }
+
+            ScriptEngine se = null;
+            var t = new Thread(() => {
+                se = new ScriptEngine();
+                se.Execute(this._uglifyJsCode);
+            }, 10 * 1048576);
+
+            t.Start();
+            t.Join();
+
+            return se;
+        }
+    }
+}
