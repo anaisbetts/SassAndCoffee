9772b4df0e074a6e7d6432dd7ddb04615614a284
Paul Betts <paul@paulbetts.org>
2011-06-05 18:47:51 -0700

    Add V8Bridge Interface DLL

---
+++ V8Bridge.Interface/Interfaces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace V8Bridge.Interface
+{
+    public interface IV8ScriptCompiler
+    {
+        void InitializeLibrary(string libraryCode);
+        string Compile(string function, string input);
+    }
+}
