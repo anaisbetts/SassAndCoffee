a356c3b23905ee6f6af644aa82acfda0fbe36a00
gavacho <mrkenbrowning@hotmail.com>
2011-07-10 23:32:30 -0700

    Introduce CssPassthroughCompiler to support IIS6

---
+++ SassAndCoffee.Core/Compilers/CssPassthroughCompiler.cs
@@ -0,0 +1,34 @@
+namespace SassAndCoffee.Core.Compilers
+{
+    using System.IO;
+
+    // TODO: Document why this exists
+    public class CssPassthroughCompiler : ISimpleFileCompiler
+    {
+        public string[] InputFileExtensions {
+            get { return new[] {".css"}; }
+        }
+
+        public string OutputFileExtension {
+            get { return ".css"; }
+        }
+
+        public string OutputMimeType {
+            get { return "text/css"; }
+        }
+
+        public void Init(ICompilerHost host)
+        {
+        }
+
+        public string ProcessFileContent(string inputFileContent)
+        {
+            return File.ReadAllText(inputFileContent);
+        }
+
+        public string GetFileChangeToken(string inputFileContent)
+        {
+            return "";
+        }
+    }
+}
