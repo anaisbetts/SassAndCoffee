7db6b953a470ca7f99c722a32ffd9d1d0395c23b
Paul Betts <paul@paulbetts.org>
2011-07-10 22:20:29 -0700

    Fix InMemoryCache

--- SassAndCoffee.Core/Caching/InMemoryCache.cs
+++ SassAndCoffee.Core/Caching/InMemoryCache.cs
@@ -1,20 +1,38 @@
 namespace SassAndCoffee.Core.Caching
 {
     using System;
-    using System.Collections.Concurrent;
+    using System.Collections.Generic;
 
     public class InMemoryCache : ICompiledCache
     {
-        readonly ConcurrentDictionary<string, CompilationResult> _cache = new ConcurrentDictionary<string, CompilationResult>();
+        readonly MemoizingMRUCache<string, CompilationResult> _cache;
+        readonly Dictionary<string, Func<string, CompilationResult>> _delegateIndex = new Dictionary<string, Func<string, CompilationResult>>();
+
+        public InMemoryCache()
+        {
+            _cache = new MemoizingMRUCache<string, CompilationResult>((file, _) => {
+                Func<string, CompilationResult> compiler = null;
+                lock (_delegateIndex) {
+                    compiler = _delegateIndex[file];
+                }
+
+                return compiler(file);
+            }, 50);
+        }
 
         public CompilationResult GetOrAdd(string filename, Func<string, CompilationResult> compilationDelegate, string mimeType)
         {
-            return _cache.GetOrAdd(filename, compilationDelegate);
+            lock (_delegateIndex) {
+                _delegateIndex[filename] = compilationDelegate;
+            }
+
+            return _cache.Get(filename);
         }
 
         public void Clear()
         {
-            _cache.Clear();
+            lock(_delegateIndex) { _delegateIndex.Clear(); }
+            _cache.InvalidateAll();
         }
     }
 }
