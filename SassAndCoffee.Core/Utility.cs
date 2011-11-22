7caaf22cb0f3beb7c4bbf8c9dc655fbc9e88c178
Nick Sivo <nick@kogir.com>
2011-11-21 17:02:34 -0800

    Streamline resource loading.

--- SassAndCoffee.Core/Utility.cs
+++ SassAndCoffee.Core/Utility.cs
@@ -1,20 +1,13 @@
-﻿namespace SassAndCoffee.Core
-{
+﻿namespace SassAndCoffee.Core {
     using System.IO;
     using System.Reflection;
     using System.Text;
 
-    public class Utility
-    {
-        public static string ResourceAsString(string resource, Assembly assembly = null, Encoding enc = null)
-        {
-            assembly = assembly ?? Assembly.GetExecutingAssembly();
-            enc = enc ?? Encoding.UTF8;
-
-            using(var ms = new MemoryStream()) {
-                assembly.GetManifestResourceStream(resource).CopyTo(ms);
-                return enc.GetString(ms.GetBuffer()).Replace('\0', ' ').Trim();
-            }
+    public class Utility {
+        public static string ResourceAsString(string resource) {
+            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+            using (var reader = new StreamReader(resourceStream))
+                return reader.ReadToEnd();
         }
     }
 }
