13f9898e62934d45fe6198bc9818bc2f262ce546
Nick Sivo <nick@kogir.com>
2011-12-19 19:23:31 -0800

    Add support for strong name signing.

--- .nuget/Publish.bat
+++ .nuget/Publish.bat
@@ -1,15 +1,28 @@
 @ECHO OFF
 
+REM Project
 SET PROJECT=%1
-
+IF NOT EXIST %PROJECT% (
+	ECHO Couldn't locate project!
+	EXIT /B -1
+)
 ECHO Using project %PROJECT%
 
+REM Nuget
 SET NUGET=%~dp0NuGet.exe
 IF NOT EXIST %NUGET% (
 	ECHO Couldn't locate NuGet.exe!
 	EXIT /B -1
 )
 ECHO Using NuGet at %NUGET%
+
+REM Strong Name Key
+SET KEYFILE=%~dp0..\SassAndCoffee.pfx
+IF NOT EXIST %KEYFILE% (
+	ECHO Couldn't locate SassAndCoffee.pfx!
+	EXIT /B -1
+)
+ECHO Using signing key at %KEYFILE%
 
 REM Check for changes
 CALL git rev-parse --verify HEAD >NUL
@@ -19,32 +32,23 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 
 CALL git update-index -q --ignore-submodules --refresh
-SET ERROR = 0
 
 CALL git diff-files --quiet --ignore-submodules
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO Abort: You have unstaged changes.
-	SET ERROR = 1
+	EXIT /B -1
 )
 
 CALL git diff-index --cached --quiet --ignore-submodules HEAD --
 IF %ERRORLEVEL% NEQ 0 (
-	IF %ERROR% EQU 0 (
-		ECHO Abort: Your index contains uncommitted changes.
-	) ELSE (
-		ECHO Additionally, your index contains uncommitted changes.
-	)
-	SET ERROR = 1
-)
-
-IF %ERROR% NEQ 0 (
+	ECHO Abort: Your index contains uncommitted changes.
 	EXIT /B -1
 )
 
 ECHO Everything looks good so far.
 CHOICE /M "About to purge. Continue?"
 IF %ERRORLEVEL% NEQ 1 (
-	Echo Aborting.
+	ECHO Aborting.
 	EXIT /B -1
 )
 
@@ -53,23 +57,17 @@ CALL git clean -x -d -f >NUL 2>&1
 ECHO Purge complete.
 
 REM Build Package
-%NUGET% pack %1 -Properties "Configuration=Release" -verbose -symbols -build
+%NUGET% pack %1 -Properties "Configuration=Release;SignAssembly=true;AssemblyOriginatorKeyFile=%KEYFILE%" -verbose -symbols -build
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO Unable to create package!  Abort!
 	EXIT /B -1
 )
 
-REM Tag and publish release
+REM Publish release
 FOR %%P IN (*.nupkg) DO (
-	REM hg tag "%%~nP" -m "Recording source for NuGet package" -r . > NUL 2>&1
-	REM IF %ERRORLEVEL% NEQ 0 (
-	REM 	ECHO Unable to tag!  Abort!
-	REM 	EXIT /B -1
-	REM )
-	REM ECHO Created tag %%~nP
 	CHOICE /M "Continue pushing package: '%%~nxP'?"
-	IF %ERRORLEVEL% NEQ 0 (
-		Echo Aborting.
+	IF %ERRORLEVEL% NEQ 1 (
+		ECHO Aborting.
 		EXIT /B -1
 	)
 	%NUGET% push "%%~nxP"
